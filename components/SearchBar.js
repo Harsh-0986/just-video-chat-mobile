477347040f07092d1e3f35640ab2b1652b156d66
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-10-05 10:19:12 +0530

    Completed SearchBar design

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Fontisto from "react-native-vector-icons/Fontisto";
 
 export default function SearchBar() {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View style={styles.container}>
+      {/* Adding Search Bar Icon */}
+      <Fontisto name="search" size={20} color="#858585" />
+      <Text style={styles.textSearchBar}>Search</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#333333",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    height: 40,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  textSearchBar: {
+    color: "#858585",
+    paddingLeft: 10,
+    fontSize: 20,
+  },
+});
