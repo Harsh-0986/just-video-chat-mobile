d09480804e20b8e12bf6dcfc2367e86042401de8
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-10-05 10:10:04 +0530

    Completed header

---
+++ components/ContactsMenu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ContactsMenu() {
+  return (
+    <View>
+      <Text>ContactsMenu</Text>
+    </View>
+  );
+}
---
+++ screens/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import ContactsMenu from "../components/ContactsMenu";
+import Header from "../components/Header";
+import MenuButtons from "../components/MenuButtons";
+import SearchBar from "../components/SearchBar";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView
+        style={{
+          minHeight: `${Platform.OS === "web" ? "96.5vh" : "100%"}`,
+        }}
+      >
+        {/* Header */}
+        <Header />
+        {/* SearchBar */}
+        <SearchBar />
+        {/* Menu */}
+        <MenuButtons />
+        {/* Contacts */}
+        <ContactsMenu />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1c1c1c",
+    padding: 15,
+  },
+});
