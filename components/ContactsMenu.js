93351cff391ed215306d8270f2a42bf31ca60b81
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-10-06 11:58:28 +0530

    Completed Contact Menu design

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function ContactsMenu() {
   return (
-    <View>
-      <Text>ContactsMenu</Text>
+    <View style={styles.container}>
+      {/* Contact Container */}
+      <View style={styles.row}>
+        {/* Image Container */}
+        <View style={styles.starredIcon}>
+          <AntDesign name="star" size={30} color="#efefef" />
+        </View>
+        {/* Text */}
+        <Text style={styles.text}>Starred</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  row: {
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+  },
+  starredIcon: {
+    backgroundColor: "#333333",
+    width: 55,
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  text: {
+    color: "white",
+    paddingLeft: 15,
+    fontSize: 18,
+  },
+});
